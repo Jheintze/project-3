5426cd5bb603695e0ad3cd9dfcf062e78f52de53
Jakob <jheintze33@gmail.com>
2022-12-07 15:45:52 +0100

    formtoaddplanetintodatabase

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@
 
 import Home from "./pages/home/home"
 import { Routes, Route } from "react-router-dom";
-// import AddProject from "./components/AddProject";
+import AddPlanet from "./components/AddPlanet.js";
 
 // import IsPrivate from "./components/IsPrivate";
 // import IsAnon from "./components/IsAnon";
 
 function App() {
   return <div className="App">
-    
     <h2>
   <Routes>
   <Route path="/" element={<Home />} />
+  <Route path="/planets" element={<AddPlanet />} />
   
 
   </Routes>
