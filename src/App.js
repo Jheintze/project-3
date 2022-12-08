2a29815134307535224dcf625ea479feca079dfd
Jakob <jheintze33@gmail.com>
2022-12-08 14:08:55 +0100

    add navbar, add implement dropdown

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@
 import Home from "./pages/home/home"
 import { Routes, Route } from "react-router-dom";
 import AddPlanet from "./components/AddPlanet.js";
-
+import Navbar from "./components/Navbar/Navbar";
+import Destinations from "./pages/destinations/destinations"
 // import IsPrivate from "./components/IsPrivate";
 // import IsAnon from "./components/IsAnon";
 
 function App() {
   return <div className="App">
-    <h2>
+  <Navbar/>
+    
   <Routes>
   <Route path="/" element={<Home />} />
   <Route path="/planets" element={<AddPlanet />} />
-  
-
-  </Routes>
+  <Route path="/destinations/:id" element={<Destinations/>} />
+ </Routes>
     
-     </h2>
+     
   </div>;
 }
 export default App;
